39b9473bb86a98760b913a3a0134ee84ece891b0
tanharr <kharismawan71@gmail.com>
2023-10-08 18:57:16 +0700

    first commit

---
+++ Bab1/index.php
@@ -0,0 +1,39 @@
+<!-- 1. Membuat struktur HTML -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan HTML</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias error dolorem nostrum eum aliquam dignissimos soluta incidunt quod consequatur atque repellat, dolor vitae nihil repellendus? Dolorem unde debitis fugiat cum.</p>
+</body>
+</html>
+
+
+<!-- 2. Belajar PHP -->
+
+<?php
+    $kata = "Kharismawan Tanhar";
+    echo "$kata";
+?>
+
+<!-- 3. Belajar Kondisi -->
+
+<?php
+$nilai = 69;
+if($nilai >= 90 && $nilai = 100){
+    echo 'Nilai Anda A';
+}
+elseif($nilai >= 80 && $nilai <= 89){
+    echo 'Nilai Anda B';
+}
+elseif($nilai >= 70 && $nilai <= 79){
+    echo 'Nilai Anda C';
+}
+else{
+    echo 'Nilai Anda D';
+}
+?>
